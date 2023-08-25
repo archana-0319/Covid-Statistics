d2819204a40aaf6bce439f6d949af915a59f4b90
Archana <archanasreenivas.k@gmail.com>
2023-08-25 13:22:39 +0530

    final commit

--- src/connector.js
+++ src/connector.js
@@ -1,14 +1,14 @@
 
 //const mongodb = require('mongodb');
 
-// const mongoURI = process.env.MONGODB_URL;
+const mongoURI = process.env.MONGODB_URL;
 
 let mongoose = require('mongoose');
 require('dotenv').config();
 const { tallySchema } = require('./schema')
 
 
-mongoose.connect("mongodb+srv://archanasreenivask:covidtally-data@cluster0.kdlvrr9.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         // console.log("error while connection", err)
