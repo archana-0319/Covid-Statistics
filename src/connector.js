7674c2cb1e6963cc72b9f3771c2898273b5e2c53
Archana <archanasreenivas.k@gmail.com>
2023-08-25 13:26:37 +0530

    done

--- src/connector.js
+++ src/connector.js
@@ -11,8 +11,8 @@ const { tallySchema } = require('./schema')
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
-        // console.log("error while connection", err)
-        console.log("error while connection")
+        console.log("error while connection", err)
+        // console.log("error while connection")
     });
 collection_connection = mongoose.model('covidtally', tallySchema)
 
